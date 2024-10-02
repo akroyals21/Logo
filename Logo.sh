2f932d9fb012c6ca32782225bd7a89d1ee469a78
Akash raj <mustanda.aiwc@gmail.com>
2024-10-02 15:42:49 +0530

    Update Logo.sh

--- Logo.sh
+++ Logo.sh
@@ -1,13 +1,20 @@
-#!/bin/bash
+echo -e "\e[1;34m
+ 
+ #Twitter: (https://x.com/mustanda2302)
 
-# Introductory ASCII art displayed in blue
-echo -e "\e[1;34m \
- ▗▄▖ ▗▖ ▗▖▗▄▄▖  ▗▄▖▗▖  ▗▖▗▄▖ ▗▖    ▗▄▄▖ \
-▐▌ ▐▌▐▌▗▞▘▐▌ ▐▌▐▌ ▐▌▝▚▞▘▐▌ ▐▌▐▌   ▐▌   \
-▐▛▀▜▌▐▛▚▖ ▐▛▀▚▖▐▌ ▐▌ ▐▌ ▐▛▀▜▌▐▌    ▝▀▚▖ \
-▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌▝▚▄▞▘ ▐▌ ▐▌ ▐▌▐▙▄▄▖▗▄▄▞▘ \
-\e[0m"
+ ▗▄▖ ▗▖ ▗▖▗▄▄▖  ▗▄▖▗▖  ▗▖▗▄▖ ▗▖    ▗▄▄▖
+▐▌ ▐▌▐▌▗▞▘▐▌ ▐▌▐▌ ▐▌▝▚▞▘▐▌ ▐▌▐▌   ▐▌   
+▐▛▀▜▌▐▛▚▖ ▐▛▀▚▖▐▌ ▐▌ ▐▌ ▐▛▀▜▌▐▌    ▝▀▚▖
+▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌▝▚▄▞▘ ▐▌ ▐▌ ▐▌▐▙▄▄▖▗▄▄▞▘                              
+                                       
+                                    
+                                       
+                                                                           
+                                       
+\e[0m"  # Reset color
 
-# Function to display an image from a URL
-curl -s https://avatars.githubusercontent.com/u/136575299?v=4 -o avatar.jpg
-echo -e "\e[1;34mImage downloaded as avatar.jpg. Use an image viewer to open it.\e[0m"
+echo -e "\e[1;34m  
+◤━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━◥
+◣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━◢
+      
+\e[0m"  # Reset color
